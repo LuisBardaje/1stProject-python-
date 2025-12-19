42ab2f2f12f67e4d10c954561c06890ae8b04cb5
LuisBardaje <marcluisbardaje123@gmail.com>
2025-12-19 14:30:20 +0800

    new changes

--- mizelda.py
+++ mizelda.py
@@ -1,2 +1,12 @@
 import pygame
-from sys import exit
+
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Mizelda')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
