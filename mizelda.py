815ff652fab4a1a970fff20b589ea185681e7a53
LuisBardaje <marcluisbardaje123@gmail.com>
2025-12-22 11:39:17 +0800

    debug python

--- mizelda.py
+++ mizelda.py
@@ -1,12 +1,30 @@
-import pygame
+import pygame, sys
+from settings import *
+from level import Level
 
 
-pygame.init()
-screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Mizelda')
+class Game:
+    def __init__(self):
+        
+        pygame.init()
+        self.screen = pygame.display.set_mode((800,400))
+        pygame.display.set_caption('Mizelda')
+        self.clock = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+        self.level = Level()
+
+    def run(self):  
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
