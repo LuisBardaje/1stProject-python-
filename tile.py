056593c7317702b80b98b5d9ca68b2ec70cb0345
LuisBardaje <marcluisbardaje123@gmail.com>
2025-12-23 13:01:06 +0800

    tiles update

--- tile.py
+++ tile.py
@@ -4,5 +4,5 @@ import pygame
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('tilesets/tiles.png').convert_alpha()
+        self.image = pygame.image.load('tilesets/rocktile.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)  
