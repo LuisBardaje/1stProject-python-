2e113c8f25c81c702a100917faf2d8f0516a46f5
LuisBardaje <marcluisbardaje123@gmail.com>
2026-01-09 13:41:38 +0800

    map changes

---
+++ tilesets/TilesetDesert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TilesetDesert" tilewidth="16" tileheight="16" tilecount="240" columns="20" tilerendersize="grid">
+ <image source="../../gameassets/Ninja Adventure - Asset Pack/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetDesert.png" trans="00ffff" width="320" height="192"/>
+</tileset>
---
+++ tilesets/TilesetWater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TilesetWater" tilewidth="16" tileheight="16" tilecount="476" columns="28" tilerendersize="grid">
+ <image source="../../gameassets/Ninja Adventure - Asset Pack/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetWater.png" trans="000000" width="448" height="272"/>
+</tileset>
